a363a8038b86446946d69d154820e2116dafd2de
maxbarsukov <maximbarsukov@bk.ru>
2023-11-13 13:35:00 +0300

    feat: :sparkles: basic Spring Boot app

---
+++ src/main/java/ru/itmo/web/lab4/bootloader/Lab4Application.java
@@ -0,0 +1,11 @@
+package ru.itmo.web.lab4.bootloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4Application.class, args);
+	}
+}
